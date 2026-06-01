bfc66f33739110e8178210803434324b3c269d6d
victoryacrasnova-star <victoryacrasnova@yandex.ru>
2026-06-01 22:43:43 +0300

    add database configuration

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from app.database import engine
+
+with engine.connect() as connection:
+    connection.execute(text("SELECT 1"))
 app = FastAPI(title="Job Queue API")
 
 @app.get("/health")
