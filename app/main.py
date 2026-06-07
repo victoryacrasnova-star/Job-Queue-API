66dddb6865b8b7229cfc21e2298902f6a0b5d769
victoryacrasnova-star <victoryacrasnova@yandex.ru>
2026-06-07 20:05:32 +0300

    add router job

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from sqlalchemy import text
 
-from app.database import engine
+from app.routers import job
 
-with engine.connect() as connection:
-    connection.execute(text("SELECT 1"))
 app = FastAPI(title="Job Queue API")
+app.include_router(job.router)
 
 @app.get("/health")
 def health_check():
---
+++ app/routers/job.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.schemas import JobRead, JobCreate
+from app.crud import create_job as create_job_crud
+from app.crud import get_job as get_job_crud
+from app.crud import list_jobs as list_jobs_crud
+
+router = APIRouter(prefix="/jobs", tags=["jobs"])
+
+@router.post("", response_model=JobRead, status_code=status.HTTP_201_CREATED)
+def create_job(job_data: JobCreate, db: Session = Depends(get_db)) -> JobRead:
+    return create_job_crud(db, job_data)
+
+@router.get("",response_model=list[JobRead])
+def get_jobs(db: Session = Depends(get_db)) -> list[JobRead]:
+    return list_jobs_crud(db)
+
+@router.get("/{job_id}", response_model=JobRead)
+def get_job_by_id(job_id: int, db: Session = Depends(get_db)) -> JobRead:
+    job = get_job_crud(db, job_id)
+
+    if job is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Job not found")
+
+    return job
