884ba005fd055089190d048648152aca630a69b7
victoryacrasnova-star <victoryacrasnova@yandex.ru>
2026-07-15 02:46:51 +0300

    add test_main

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient
+
+def test_chek():
+    
+    #act
+    
+    response = client.get("/health")
+
+    #assert
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
